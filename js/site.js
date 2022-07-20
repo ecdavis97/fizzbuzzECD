6a59a6cd85bb02b97795a27eb9ca62ced3087498
ecdavis97 <ethancdavis97@gmail.com>
2022-07-19 22:23:04 -0400

    fizzbuzz app

--- js/site.js
+++ js/site.js
@@ -1,17 +1,37 @@
-function getValue(){
-    alert("Hello World!")
-}
+document.getElementById("btnSubmit").addEventListener("click", FizzBuzz);
+function FizzBuzz(){
+    //get references to the inputs
+    let fizz = document.getElementById("fizzNum");
+    let buzz = document.getElementById("buzzNum");
 
-function getValue2(){
-    let msg='';
+    //parse the numbers --> make sure the inputs are NUMBERS
+    let fizzNum = parseInt(fizz.value);
+    let buzzNum = parseInt(buzz.value);
 
-    msg=document.getElementById("message").value;
+    let displayText = "";
+    let fizzResult, buzzResult = 0;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
+    //do something with the inputs
+    for (let loop = 1; loop <= 100; loop++){
+        //1 2 3 4...100
+        //fizzNum = 3
+        //buzzNum = 5
+        //
+        fizzResult = loop % fizzNum; //modulus operator gives you the remainder after dividing by itself
+        buzzResult = loop % buzzNum; //this is the modulus of the second number 1 % 5 = 5, 2 % 5 = 5... 5 % 5 =0, 6 % 5 = 1
+        
+        if(fizzResult == 0 && buzzResult == 0){
+            displayText += "FIZZBUZZ, "
         }
-    );
+        else if(fizzResult == 0){
+            displayText += "FIZZ, ";
+        }
+        else if(buzzResult == 0){
+            displayText += "BUZZ, "
+        }
+        else{
+            displayText += `${loop}, `
+        }
+    }
+    document.getElementById("results").innerText = displayText;
 }
